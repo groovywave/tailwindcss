6e1e2c67ff4d240f726af51590edd35fa5ba4d06
groove <vacation555@gmail.com>
2023-11-07 00:42:51 +0900

    feat:add nav menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       colors: {
         body: "#050505",
+        "selected-text": "#a3a3ff",
+        theme: "#5c318c",
         google: {
           googleBlue: "#4285f4",
           googleGreen: "#34a853",
@@ -19,33 +21,3 @@ module.exports = {
   },
   plugins: [],
 };
-/** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./src/**/*.{html,js}"],
-//   theme: {
-//     colors: {
-//       blue: "#1fb6ff",
-//       purple: "#7e5bef",
-//       pink: "#ff49db",
-//       orange: "#ff7849",
-//       green: "#13ce66",
-//       yellow: "#ffc82c",
-//       "gray-dark": "#273444",
-//       gray: "#8492a6",
-//       "gray-light": "#d3dce6",
-//     },
-//     fontFamily: {
-//       sans: ["Graphik", "sans-serif"],
-//       serif: ["Merriweather", "serif"],
-//     },
-//     extend: {
-//       spacing: {
-//         128: "32rem",
-//         144: "36rem",
-//       },
-//       borderRadius: {
-//         "4xl": "2rem",
-//       },
-//     },
-//   },
-// };
